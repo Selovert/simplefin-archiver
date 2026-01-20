d7d769502c71eb30059e5da4e8942f7bf4157f14
Tassilo Selover-Stephan <tassilo@selover-stephan.com>
2026-01-19 19:15:43 -0800

    drop docker stuff & prefect flows

--- flows/serve.py
+++
@@ -1,8 +0,0 @@
-from update_accounts import update_accounts
-
-if __name__ == "__main__":
-    update_accounts.serve(
-        name="simplefin-update-accounts",
-        tags=["simplefin"],
-        cron="45 3 * * *"
-    )
